2bd4d2f983320e89d5fc54bba839c96ec3a48ca1
kmk <khinmonkyaw95@gmail.com>
2018-06-02 01:32:25 +0630

    project skeleton KMK

---
+++ app/src/main/java/com/padcmyanmer/ted/adapters/NewsAdapter.java
@@ -0,0 +1,29 @@
+package com.padcmyanmer.ted.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.padcmyanmer.ted.R;
+import com.padcmyanmer.ted.viewholders.NewsViewHolder;
+
+public class NewsAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view=layoutInflater.inflate(R.layout.view_holder_news1,parent,false);
+        return new NewsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+}
